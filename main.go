d54fa5e455f5e13973aac38ccd60a6693e1c5cbd
Amrit poudel <amritpoudel433@gmail.com>
2024-06-20 23:56:32 +0545

    Initialize the body

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func encrypt(key int, plainText string) (result string) {
+	return
+
+}
+
+func decpryt(key int, encryptedText string) (result string) {
+	return
+}
 func main() {
-	fmt.Println("Hello World!")
+
+	reader := bufio.NewReader(os.Stdin)
+	//Get the user input for the encryption
+	fmt.Println("Enter the plain text for encryption")
+	plainText, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	encrypted := encrypt(5, plainText)
+	fmt.Println("The plain text is ", encrypted)
+
+	decryptedText := decpryt(5, encrypted)
+	fmt.Println("The plain text is ", decryptedText)
+
 }
